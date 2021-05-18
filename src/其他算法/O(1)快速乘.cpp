22b0100c8f4586c9a80dd0cd1e4918e287c0fce1
AntiLeaf <antileaf@163.com>
2021-05-18 21:08:43 +0800

    update tex

--- src/其他算法/O(1)快速乘.cpp
+++ src/其他算法/O(1)快速乘.cpp
@@ -1,9 +1,6 @@
-﻿// Quick Multiplication O(1)快速乘
-// By ysf
-
-// long double 快速乘
+﻿// long double 快速乘
 // 在两数直接相乘会爆long long时才有必要使用
-// 常数比直接long long乘法+取模大很多，非必要时不建议使用
+// 常数比直接long long乘法+取模大很多,非必要时不建议使用
 long long mul(long long a,long long b,long long p){
 	a%=p;b%=p;
 	return ((a*b-p*(long long)((long double)a/p*b+0.5))%p+p)%p;
---
+++ src/其他算法/xorshift.cpp
@@ -0,0 +1,27 @@
+ull k1, k2;
+const int mod = 10000000; 
+ull xorShift128Plus() {
+    ull k3 = k1, k4 = k2;
+    k1 = k4;
+    k3 ^= (k3 << 23);
+    k2 = k3 ^ k4 ^ (k3 >> 17) ^ (k4 >> 26);
+    return k2 + k4;
+}
+void gen(ull _k1, ull _k2) {
+    k1 = _k1, k2 = _k2;
+    int x = xorShift128Plus() % threshold + 1;
+    // do sth
+}
+
+
+uint32_t xor128(void) {
+    static uint32_t x = 123456789;
+    static uint32_t y = 362436069;
+    static uint32_t z = 521288629;
+    static uint32_t w = 88675123;
+    uint32_t t;
+​
+    t = x ^ (x << 11);
+    x = y; y = z; z = w;
+    return w = w ^ (w >> 19) ^ (t ^ (t >> 8));
+}
