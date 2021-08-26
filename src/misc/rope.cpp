f6538df53c6e73d04860b1796f940fafe64f3f15
AntiLeaf <antileaf@163.com>
2021-08-26 23:26:08 +0800

    8.26 fix

--- src/misc/rope.cpp
+++ src/misc/rope.cpp
@@ -3,7 +3,7 @@ using namespace __gnu_cxx;
 
 push_back(x); // 在末尾添加x
 insert(pos, x); // 在pos插入x, 自然支持整个char数组的一次插入
-erase(pos, x); // 从pos开始删除x个
+erase(pos, x); // 从pos开始删除x个, 不要只传一个参数, 有毒
 copy(pos, len, x); // 从pos开始到pos + len为止的部分, 赋值给x
 replace(pos, x); // 从pos开始换成x
 substr(pos, x); // 提取pos开始x个
