a001452ff70a0339addf6cae51c26c65e6aab752
AntiLeaf <antileaf@163.com>
2022-02-21 05:29:31 +0800

    2.21 added reference & changed titlepage

---
+++ src/datastructure/二叉堆.cpp
@@ -0,0 +1,45 @@
+struct my_binary_heap {
+	static constexpr int maxn = 100005;
+
+	int a[maxn], size;
+
+	my_binary_heap() : size(0) {}
+
+	void push(int val) {
+		a[++size] = val;
+
+		for (int x = size; x > 1; x /= 2) {
+			if (a[x] < a[x / 2])
+				swap(a[x], a[x / 2]);
+			else
+				break;
+		}
+	}
+
+	int& top() {
+		return a[1];
+	}
+
+	int pop() {
+		int res = a[1];
+		a[1] = a[size--];
+
+		for (int x = 1, son; ; x = son) {
+			if (x * 2 == size)
+				son = x * 2;
+			else if (x * 2 > size)
+				break;
+			else if (a[x * 2] < a[x * 2 + 1])
+				son = x * 2;
+			else
+				son = x * 2 + 1;
+
+			if (a[son] < a[x])
+				swap(a[x], a[son]);
+			else
+				break;
+		}
+		
+		return res;
+	}
+};
