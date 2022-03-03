cf0756fb2f8b543b19019a6ab4f7a513dbabf8ff
AntiLeaf <antileaf@163.com>
2022-03-04 02:31:47 +0800

    3.4 added Berlekamp-Massey

---
+++ src/math/Berlekamp-Massey.cpp
@@ -0,0 +1,50 @@
+vector<int> berlekamp_massey(const vector<int> &a) {
+	vector<int> v, last; // v is the answer, 0-based
+	int k = -1, delta = 0;
+
+	for (int i = 0; i < (int)a.size(); i++) {
+
+		int tmp = 0;
+		for (int j = 0; j < (int)v.size(); j++)
+			tmp = (tmp + (long long)a[i - j - 1] * v[j]) % p;
+		
+		if (a[i] == tmp)
+			continue;
+		
+		if (k < 0) {
+			k = i;
+			delta = (a[i] - tmp + p) % p;
+			v = vector<int>(i + 1);
+
+			continue;
+		}
+
+		vector<int> u = v;
+		int val = (long long)(a[i] - tmp + p) * qpow(delta, p - 2) % p;
+
+		if (v.size() < last.size() + i - k)
+			v.resize(last.size() + i - k);
+		
+		(v[i - k - 1] += val) %= p;
+		
+		for (int j = 0; j < (int)last.size(); j++) {
+			v[i - k + j] = (v[i - k + j] - (long long)val * last[j]) % p;
+			if (v[i - k + j] < 0)
+				v[i - k + j] += p;
+		}
+
+		if ((int)u.size() - i < (int)last.size() - k) {
+			last = u;
+			k = i;
+			delta = a[i] - tmp;
+			if (delta < 0)
+				delta += p;
+		}
+	}
+
+	for (auto &x : v) // 一般是需要最小递推式的, 所以处理一下
+		x = (p - x) % p;
+	v.insert(v.begin(), 1);
+
+	return v; // $\forall i, \sum_{j = 0} ^ m a_{i - j} v_j = 0$
+}
