fac8c2f27e334912680115ce0c97b7bdf8541b1a
AntiLeaf <antileaf@163.com>
2022-03-30 01:19:40 +0800

    3.30 added 欧拉回路

---
+++ src/graph/欧拉回路.cpp
@@ -0,0 +1,25 @@
+vector<int> G[maxn], C[maxn], v[maxn];
+int cur[maxn];
+bool vis[maxn * 2];
+
+vector<pair<int, int> > vec;
+
+int d[maxn];
+
+void dfs(int x) {
+	bool bad = false;
+
+	while (!bad) {
+		bad = true;
+
+		for (int &i = cur[x]; i < (int)G[x].size(); i++)
+			if (!vis[C[x][i]]) {
+				vis[C[x][i]] = true;
+				vec.emplace_back(x, i);
+				x = G[x][i];
+				bad = false;
+
+				break;
+			}
+	}
+}
