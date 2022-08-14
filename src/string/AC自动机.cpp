776090d4da775f1609a5f544e8324d31a2936aa9
AntiLeaf <antileaf@163.com>
2022-08-14 12:24:31 +0800

    8.14 update aczdj

--- src/string/AC自动机.cpp
+++ src/string/AC自动机.cpp
@@ -2,39 +2,39 @@ int ch[maxm][26], f[maxm][26], q[maxm], sum[maxm], cnt = 0;
 
 // 在字典树中插入一个字符串 O(n)
 int insert(const char *c) {
-    int x = 0;
-    while (*c) {
-        if (!ch[x][*c - 'a'])
-            ch[x][*c - 'a'] = ++cnt;
-        x = ch[x][*c++ - 'a'];
-    }
-    return x;
+	int x = 0;
+	while (*c) {
+		if (!ch[x][*c - 'a'])
+			ch[x][*c - 'a'] = ++cnt;
+		x = ch[x][*c++ - 'a'];
+	}
+	return x;
 }
 
 // 建AC自动机 O(n * sigma)
 void getfail() {
-    int x, head = 0, tail = 0;
+	int x, head = 0, tail = 0;
 
-    for (int c = 0; c < 26; c++)
-        if (ch[0][c])
-            q[tail++] = ch[0][c]; // 把根节点的儿子加入队列
-    
-    while (head != tail) {
-        x = q[head++];
-        
-        G[f[x][0]].push_back(x);
-        fill(f[x] + 1, f[x] + 26, cnt + 1);
+	for (int c = 0; c < 26; c++)
+		if (ch[0][c])
+			q[tail++] = ch[0][c]; // 把根节点的儿子加入队列
+	
+	while (head != tail) {
+		x = q[head++];
+		
+		G[f[x][0]].push_back(x);
+		fill(f[x] + 1, f[x] + 26, cnt + 1);
 
-        for (int c = 0; c < 26; c++) {
-            if (ch[x][c]) {
-                int y = f[x][0];
+		for (int c = 0; c < 26; c++) {
+			if (ch[x][c]) {
+				int y = f[x][0];
 
-                f[ch[x][c]][0] = ch[y][c];
-                q[tail++] = ch[x][c];
-            }
-            else
-                ch[x][c] = ch[f[x][0]][c];
-        }
-    }
-    fill(f[0], f[0] + 26, cnt + 1);
+				f[ch[x][c]][0] = ch[y][c];
+				q[tail++] = ch[x][c];
+			}
+			else
+				ch[x][c] = ch[f[x][0]][c];
+		}
+	}
+	fill(f[0], f[0] + 26, cnt + 1);
 }
